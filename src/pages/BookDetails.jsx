ba1435668c7bd2ce226b33c459ea695b9cc02a94
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-04-04 23:08:11 +0530

    book details page

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Rating } from '../components';
+import { booksData } from '../../books.js';
 import './BookDetails.scss';
 
 function BookDetails() {
-	return <div>BookDetails</div>;
+	const { book_id } = useParams();
+	const { author, cover, genres, plot, rating, reviews, title } =
+		booksData.filter(book => book.book_id == book_id)[0];
+
+	const styles = {
+		backgroundImage: `url(${cover})`,
+	};
+
+	return (
+		<div className="container">
+			<div className="details">
+				<div className="details__cover" style={styles}>
+					&nbsp;
+				</div>
+				<div className="details__right">
+					<h2 className="heading-tertiary">{title}</h2>
+					<span className="details__author">
+						&mdash; {` ${author.first_name} ${author.last_name}`}
+					</span>
+					<Rating maxRating={5} color="#FFC300" size={2} rating={rating} />
+					<p className="details__plot">{plot}</p>
+					<div className="details__genres">
+						{genres.map(genre => (
+							<span key={genre}>{genre}</span>
+						))}
+					</div>
+					<div>buttons</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default BookDetails;
