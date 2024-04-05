5f23a39cd8f8e3438eff7b02dcfe4cb106fe2a59
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-04-05 12:29:24 +0530

    book holdre and responsivness

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -8,26 +8,28 @@ function Book({ book }) {
 	const navigate = useNavigate();
 
 	return (
-		<figure
-			className="book"
-			onClick={() => navigate(`/book-details/${book_id}`)}
-		>
-			<img src={cover} alt={title} className="book__cover" />
-			<figcaption className="book__caption">
-				<div className="book__main">
-					<p className="book__title">{title}</p>
-					<span className="book__author">
-						<em>{`${author.first_name} ${author.last_name}`}</em>
-					</span>
-				</div>
-				<div className="book__footer">
-					<span className="book__price">$30</span>
-					<a className="book__icon">
-						<HiArrowNarrowRight />
-					</a>
-				</div>
-			</figcaption>
-		</figure>
+		<div className="book-container">
+			<figure
+				className="book"
+				onClick={() => navigate(`/book-details/${book_id}`)}
+			>
+				<img src={cover} alt={title} className="book__cover" />
+				<figcaption className="book__caption">
+					<div className="book__main">
+						<p className="book__title">{title}</p>
+						<span className="book__author">
+							<em>{`${author.first_name} ${author.last_name}`}</em>
+						</span>
+					</div>
+					<div className="book__footer">
+						<span className="book__price">$30</span>
+						<a className="book__icon">
+							<HiArrowNarrowRight />
+						</a>
+					</div>
+				</figcaption>
+			</figure>
+		</div>
 	);
 }
 
